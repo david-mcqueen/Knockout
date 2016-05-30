3a441918311cec2b798151f32490de23f8f6b681
David McQueen <david@dmcqueen.co.uk>
2016-05-30 08:18:17 +0100

    init of basic application to learn knockout

---
+++ TaxBrackets.js
@@ -0,0 +1,63 @@
+// Class to represent a row in the TaxBracket  grid
+function TaxBracket(name, value, threshold, amountIncluded) {
+  var self = this;
+  self.name = name;
+  self.value = value;
+  self.threshold = threshold;
+  self.amountIncluded = ko.observable(amountIncluded);
+
+  self.taxPayable = ko.computed(function(){
+    return self.amountIncluded() * (value / 100);
+  });
+
+}
+
+// Overall viewmodel for this screen, along with initial state
+function TaxCalculatorViewModel() {
+    var self = this;
+    self.grossSalary = ko.observable(0);
+    self.totalTaxable = ko.observable();
+    self.totalTaxed = ko.observable();
+    self.displayTaxbracketBreakdown = ko.observable(true);
+
+    self.grossSalary.subscribe(function(newValue){
+      self.calculateBracketBreakdown(newValue);
+    });
+
+    self.TaxBrackets = ko.observableArray([
+        new TaxBracket("Personal Allowance", 0, 11000, 0),
+        new TaxBracket("Basic rate", 20, 43000, 0),
+        new TaxBracket("Higher rate", 40, 150000, 0),
+        new TaxBracket("Additional rate", 45, 99999999, 0)
+    ]);
+
+    self.calculateBracketBreakdown = function(salary) {
+      self.totalTaxed(0);
+      self.totalTaxable(0);
+
+      ko.utils.arrayForEach(self.TaxBrackets(), function(bracket){
+
+        if (salary > bracket.threshold){
+          bracket.amountIncluded(bracket.threshold);
+          self.totalTaxable(self.totalTaxable() + bracket.threshold);
+          salary -= bracket.threshold;
+        } else{
+          bracket.amountIncluded(salary);
+          self.totalTaxable(self.totalTaxable() + parseFloat(salary));
+          salary -= salary;
+        }
+
+        self.totalTaxed(self.totalTaxed() + parseFloat(bracket.taxPayable()));
+
+        if (bracket.name === "Personal Allowance"){
+          self.totalTaxable(self.totalTaxable() - bracket.amountIncluded());
+        }
+      });
+    }
+
+
+}
+
+
+
+ko.applyBindings(new TaxCalculatorViewModel());
